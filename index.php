503622faf35d66d29048c2f6107bcf93216195f7
Raffael <github@raffaeljesche.de>
2018-11-28 19:09:15 +0100

    cleanup, better output, more test host screenshots

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <!doctype html>
-<html><head><title>SimpleImage Tests</title>
-<style>
-html{background:#eee;}
-th, td {
-    border-right: 1px solid #000;
-    padding: .2em;
-    text-align: center;
-}
-img{margin:.5em;vertical-align:middle;}
-img.original{
-    margin-right: 1em;
-    padding: .5em;
-    border: 1px solid #ccc;
-}
-div{border-bottom: 4px solid #ccc;}
-</style></head><body>
+<html><head><title>SimpleImage Tests</title><link rel="stylesheet" href="style.css"></head><body>
 <?php
 // write some system info on top of page
 $info = [
@@ -28,16 +13,8 @@ $info = [
 ];
 ?>
 <table>
-    <tr>
-    <?php foreach ($info as $key => $val) {
-        echo "<th>$key</th>\r\n";
-    } ?>
-    </tr>
-    <tr>
-    <?php foreach ($info as $key => $val) {
-        echo "<td>$val</td>\r\n";
-    } ?>
-    </tr>
+    <tr><?php foreach ($info as $key => $val) { echo "<th>$key</th>\r\n"; } ?></tr>
+    <tr><?php foreach ($info as $key => $val) { echo "<td>$val</td>\r\n"; } ?></tr>
 </table>
 
 <?php
@@ -46,29 +23,32 @@ $info = [
 require_once('SimpleImage.php');
 
 // input and output dirs
-$input = 'input/';
-$output = 'output/';
+$input = 'input';
+$output = 'output';
 
 // tests
 $tests = [
 
 //  ['method' => ['arg1', 'arg2']  ],
   
-    ['crop'   => [10, 10, 40, 40]],
-    ['rotate' => [10]],
-    ['rotate' => [45]],
-    ['rotate' => [90]],
-    ['rotate' => [180]],
-    ['rotate' => [45, 'green']],
-    ['resize' => [40, 80]],
+    ['crop'       => [10, 10, 40, 40]],
+    ['resize'     => [40, 80]],
     
-    ['bestFit' => [40, 80]],
-    ['thumbnail' => [60, 60, 'center']],
+    ['bestFit'    => [40, 80]],
+    ['thumbnail'  => [60, 60, 'center']],
     
-    ['sharpen' => [10]],
-    ['sketch' => []],
+    ['rotate'     => [10]],
+    ['rotate'     => [45]],
+    ['rotate'     => [90]],
+    // ['rotate'     => [180]],
+    ['rotate'     => [45, 'green']],
+    
+    ['sharpen'    => [10]],
+    ['sketch'     => []],
     
 ];
+
+/******************* Test Output *******************/
 
 if (!is_dir($output))
     mkdir ($output, 0755);// create output dir
@@ -77,51 +57,63 @@ $error = '';
 
 $dir = new DirectoryIterator($input);
 
+
+// sort DirectoryIterator manually to iterate alphabetically
+$sorted = [];
 foreach ($dir as $file) {
-  
     if ($file->isDot() || $file->isDir()) continue;
-    
+    $sorted[] = new \SPLFileObject($file->getPathname());
+}
+usort($sorted, function($a, $b){
+    return strcmp($a->getFilename(), $b->getFilename());
+});
+
+
+// create images and html output
+foreach ($sorted as $file) {
+
     echo "<div>";
     echo "<p><b>".$file->getFilename()."</b></p>";
-    echo "<img src='".$file->getPathname()."' class='original'>";
-    
+    echo "<div><img src='".$file->getPathname()."' class='original'></div>";
+
     foreach ($tests as $test) {
+      
         foreach ($test as $call => $args) {
-            
+
             $outputFileName = "{$call}_" . implode('_', $args) . '_' . $file->getFilename();
-            
+
             try {
-              
+
                 $img = new \claviska\SimpleImage($file->getPathname());
-                
+
             } catch (\Exception $e) {
-              
-                echo $error === $e->getMessage() ? '' : '<span>' . $e->getMessage() . '</span>';
+
+                // display the same error (unsopported file) only once
+                echo $error === $e->getMessage() ? '' : '<div><span>' . $e->getMessage() . '</span></div>';
                 $error = $e->getMessage();
                 continue;
-                
+
             }
-            
+
             try {
-              
+
                 $img->$call(...$args) // splat args, requires PHP 5.6+
-                    ->toFile($output.$outputFileName);
-                
-                echo "<img src='$output/$outputFileName' alt='$outputFileName' title='$outputFileName'>";
-                
+                    ->toFile("$output/$outputFileName");
+
+                echo "<div><img src='$output/$outputFileName' alt='$outputFileName' title='$outputFileName'><span>$call</span></div>";
+
             } catch (\Exception $e) {
-              
-                echo '<span>' . $e->getMessage() . '</span>'; continue;
-                
+
+                echo '<div><span>' . $e->getMessage() . '</span></div>'; continue;
+
             }
-            
+
         }
     }
-    
-    echo "</div>";
-    
-}
 
+    echo "</div>";
+
+}
 
 ?>
 </body></html>
